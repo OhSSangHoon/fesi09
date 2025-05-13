f4ada608b28fa51ad736e8ab70392d06f7103ab9
OhSSangHoon <gnsdkaos99@naver.com>
2025-05-13 13:45:36 +0900

    메세지

--- src/app/page.test.tsx
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-
-describe('Basic Test', () => {
-  it('should work', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   const a = "a";
-  
+  const b = "b";
   return (
     <div>
       <h1>Hello World</h1>
