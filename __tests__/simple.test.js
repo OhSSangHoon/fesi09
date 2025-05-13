09f8b764003be71897d0ccd05b89f30a09af2314
OhSSangHoon <gnsdkaos99@naver.com>
2025-05-13 13:44:41 +0900

    초기 설정 테스트

---
+++ __tests__/simple.test.js
@@ -0,0 +1,5 @@
+describe('Simple test', () => {
+    it('should work', () => {
+        expect(true).toBe(true)
+    })
+})
---
+++ src/app/page.test.tsx
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom';
+
+describe('Basic Test', () => {
+  it('should work', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
